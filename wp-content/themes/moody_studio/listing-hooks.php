6f185d81cf0151d7c82d6b9b96c07aeb3e912f43
Ceeessan <Cessan__94@hotmail.com>
2024-02-27 19:04:36 +0100

    mini-justeringar, shop-page

--- wp-content/themes/moody_studio/listing-hooks.php
+++ wp-content/themes/moody_studio/listing-hooks.php
@@ -1,8 +1,21 @@
 <?php
 
+//Tar bort "Shop"-titeln
+add_filter( 'woocommerce_page_title', 'remove_shop_page_title' );
+function remove_shop_page_title( $page_title ) {
+    if ( is_shop() ) {
+        return '';
+    }
+    return $page_title;
+}
+
+//Skapar upp Banner för Shop-page.
 function show_banner_shop(){
 
-    $sale_banner_message = get_option('sale_banner_message');
+    $display_sale_banner = get_option('display_sale_banner');
+
+    if($display_sale_banner){
+        $sale_banner_message = get_option('sale_banner_message');
 
     echo "<div class='member-banner'> 
     <h2 class='member-text'> Member Exclusive </h2>
@@ -10,6 +23,9 @@ function show_banner_shop(){
     <p class='no-member-text'> Not a member? Join now to shop </p>
     </div>";
 
+    }
+
+    
 }
 
 add_action( 'woocommerce_before_shop_loop', 'show_banner_shop', 10 );

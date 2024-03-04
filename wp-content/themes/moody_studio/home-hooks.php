ad5ba510023c8442322853a2990b8d8351003f49
Ceeessan <Cessan__94@hotmail.com>
2024-03-04 20:57:43 +0100

    home

--- wp-content/themes/moody_studio/home-hooks.php
+++ wp-content/themes/moody_studio/home-hooks.php
@@ -14,3 +14,14 @@ add_action( 'woocommerce_shop_loop_item_title', 'custom_open_price_title_wrapper
 add_action( 'woocommerce_after_shop_loop_item_title', 'custom_close_price_title_wrapper', 10 );
 
 
+
+
+// Lägg till stjärn-rating
+add_filter( 'woocommerce_product_get_rating_html', 'mytheme_custom_star_rating_html', 10, 3 );
+
+function mytheme_custom_star_rating_html( $html, $rating, $count ) {
+    $label = sprintf( __( 'Rated %s out of 5', 'woocommerce' ), $rating );
+    $stars_html = '<div class="star-rating" role="img" aria-label="' . esc_attr( $label ) . '">' . wc_get_star_rating_html( $rating, $count ) . '</div>';
+
+    return $stars_html;
+}
